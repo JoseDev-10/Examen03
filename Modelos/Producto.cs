7181c1c599e10ad924022ba630c38f5c3fef2c99
OS <OS@DESKTOP-S1SQUNJ>
2024-11-11 01:44:26 -0500

    Agregar archivos de proyecto.

---
+++ Modelos/Producto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Examen03.Modelos
+{
+    public class Producto
+    {
+        public string Nombre { get; set; }
+        public decimal Precio { get; set; }
+        public string Descripcion { get; set; }
+    }
+}
---
+++ VistaModelos/ViewModelListarProductos.cs
@@ -0,0 +1,79 @@
+﻿using Examen03.Modelos;
+using Examen03.Utilitarios;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Examen03.VistaModelos
+{
+    public class ViewModelListarProductos : ViewModelBase
+    {
+        #region Propiedades
+        private string _Resultado;
+        public string Resultado
+        {
+            get { return _Resultado; }
+            set
+            {
+                _Resultado = value;
+                OnPropertyChanged(nameof(Resultado));
+            }
+        }
+        private string _Nombre;
+        public string Nombre
+        {
+            get { return _Nombre; }
+            set
+            {
+                _Nombre = value;
+                OnPropertyChanged(nameof(Nombre));
+            }
+        }
+        private decimal _Precio;
+        public decimal Precio
+        {
+            get { return _Precio; }
+            set
+            {
+                _Precio = value;
+                OnPropertyChanged(nameof(Precio));
+            }
+        }
+        private string _Descripcion;
+        public string Descripcion
+        {
+            get { return _Descripcion; }
+            set
+            {
+                _Descripcion = value;
+                OnPropertyChanged(nameof(Descripcion));
+            }
+        }
+        public ObservableCollection<Producto> Productos { get; }
+        #endregion
+
+        #region Comando
+        public ICommand AgregarProductoCommand { get; }
+
+        public ViewModelListarProductos()
+        {
+            AgregarProductoCommand = new RelayCommand(AgregarProducto);
+            Productos = new ObservableCollection<Producto>();
+        }
+        private void AgregarProducto()
+        {
+            Resultado = string.Concat(Nombre, " ", Precio.ToString(), " ", Descripcion);
+            Productos.Add(new Producto
+            {
+                Nombre = this.Nombre,
+                Precio = Convert.ToInt32(this.Precio),
+                Descripcion = this.Descripcion
+            });
+        }
+        #endregion
+    }
+}
